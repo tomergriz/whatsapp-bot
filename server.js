d1b28a6c10e20ca9d7d9343c68b5b228756d9cba
tomergriz <tomergriz@gmail.com>
2023-09-10 00:07:18 +0300

    Added debug logs to handleWebhook.js for better debugging

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const webhookController = require('./src/controllers/webhookController');
-require('./config/db');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const webhookController = require("./src/controllers/webhookController");
+require("./config/db");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/webhook', webhookController.handleWebhook);
+app.post("/webhook", webhookController.handleWebhook);
 
-app.post('*', async (req, res) => {
-  console.log('Received POST request:', req.body);  
-  res.status(200).send('OK*');
+app.post("*", async (req, res) => {
+    console.log("Received POST request:", req.body);
+    res.status(200).send("OK*");
 });
 
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+    console.log("Server running on http://localhost:3000");
 });
--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -1,29 +1,52 @@
-const User = require('../models/userModel');
-const twilio = require('twilio');
+const User = require("../models/userModel");
+const twilio = require("twilio");
 const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 exports.handleWebhook = async (req, res) => {
-  console.log('Webhook hit:', req.body);
+    console.log("Webhook hit:", req.body);
 
-  try {
-    const fromPhoneNumber = req.body.From.slice(9);  
-    const messageContent = req.body.Body.toLowerCase(); 
+    try {
+        const fromPhoneNumber = req.body.From.slice(9);
+        console.log("Sliced Phone Number: ", fromPhoneNumber);
 
-    const user = await User.findOne({ phoneNumber: fromPhoneNumber });
-    
-    if (!user) {
-      const newUser = new User({ phoneNumber: fromPhoneNumber, language: 'en', notes: [], reminders: [] });
-      await newUser.save();
-      
-      client.messages.create({
-        body: 'Welcome to WhatsApp Bot! Type "help" to see available commands.',
-        from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+972' + fromPhoneNumber
-      });
+        const messageContent = req.body.Body.toLowerCase();
+        console.log("Received Message Content: ", messageContent);
+
+        const user = await User.findOne({ phoneNumber: fromPhoneNumber });
+        console.log("Found user: ", user);
+
+        if (!user) {
+            const newUser = new User({ phoneNumber: fromPhoneNumber, language: "en", notes: [], reminders: [] });
+            const savedUser = await newUser.save();
+            console.log("New user saved: ", savedUser);
+
+            try {
+                const sentMessage = await client.messages.create({
+                    body: 'Welcome to WhatsApp Bot! Type "help" to see available commands.',
+                    from: "whatsapp:+14155238886",
+                    to: "whatsapp:+972" + fromPhoneNumber,
+                });
+                console.log("Welcome message sent: ", sentMessage);
+            } catch (twilioError) {
+                console.log("Error sending welcome message: ", twilioError);
+            }
+        } else {
+            console.log("User already exists: ", user);
+
+            try {
+                const sentMessage = await client.messages.create({
+                    body: "This is your reply from the WhatsApp Bot!",
+                    from: "whatsapp:+14155238886",
+                    to: "whatsapp:" + fromPhoneNumber,
+                });
+                console.log("Reply message sent: ", sentMessage);
+            } catch (twilioError) {
+                console.log("Error sending reply message: ", twilioError);
+            }
+        }
+    } catch (mongoError) {
+        console.log("MongoDB Error:", mongoError);
     }
-  } catch (err) {
-    console.log('Error:', err);
-  }
 
-  res.status(200).send('OK');
+    res.status(200).send("OK");
 };
